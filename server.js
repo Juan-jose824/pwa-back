a7635084737f066cfb28f664d91b06bca6760bf0
Juan-jose824 <juanjoserivera1928@gmail.com>
2025-10-24 00:05:31 -0600

    correccion en server

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors({
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   credentials: true
 }));
-app.options('*', cors());
+//app.options('*', cors());
 
 // -------------------- MongoDB --------------------
 const client = new MongoClient(process.env.MONGO_URI);
